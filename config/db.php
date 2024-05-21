222e8ea25d34aff9f3dc7335600d4442ecbe8d66
lesya <lesenka7867@gmail.com>
2024-05-21 10:44:24 +0300

    start

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=sukrxdcm_m1',
+    'username' => 'sukrxdcm',
+    'password' => '8c3rg4',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
